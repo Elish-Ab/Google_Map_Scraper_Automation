fbfa6eb7b736f15d3f11885955bf9ff8a563e316
Elisha <elishabu28@gmail.com>
2024-03-17 01:59:05 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from playwrite.sync_api import sync_playwright
+from dataclasses import dataclass, asdict, field
+import pandas as pd
+import argparse
+
+@dataclass
+class Business:
+    name: str = None
+    address: str = None
+    website: str = None
+    phone_number: str = None
+    
+#below code helps us to save the scraped data in excel and csv type 
+@dataclass
+class BusinessList:
+    business_list : list[Business] = field(default_factory=list)
+    
+    def dataframe(self):
+            return pd.json_normalize((asdict(business) for business in self.business_list), sep='')
+    
+    def save_to_excel(self, filename):
+            self.dataframe().to_excel(f'{filename}.xlsx', index=False)
+        
+    def save_to_csv(self, filename):
+            self.dataframe().to_csv(f'{filename}.csv', index=False)
+
+
+def main():
+        #to automate the browser
+        with sync_playrwright() as p:
+                browser = p.chromium.launch(headless=Flase) #headless = False because in development mode, but in production you can avoid it because by default it is true
+                page = browser.new_page()
+                
+                page.goto('https://www.google.com/maps', timeout=60000)
+                page.wait_for_timeout(5000) # because od dev mode i give timeout=5000, but in production you can give it 1
+                
+                page.locator('//input[@id="searchboxinput"]').fill(search_for)
+                page.wait_for_timeout(3000)
+                
+                page.keyboard.press('Enter')
+                page.wait_for_timeout(5000)
+                
+                listings = page.locator('//div[@role = "article"]').all()
+                print(len(listings))
+                
+                business_list = BusinessList()
+                
+                for listing in listings[:5]:
+                        
+                        listing.click()
+                        page.wait_for_timeout(5000)
+                        
+                        name_attibute = 'aria-label'
+                        address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
+                        website_xpath = '//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
+                        phone_number_xpath = '//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
+                        # review_count_xpath = '//button[@jsaction="pane.reviewChart.moreReviews"]//span'
+                        # reviews_average_xpath = '//div[@jsaction="pane.reviewChart.moreReviews"]//div[@role="img"]'
+                browser.close()
+                
+#the below code/class allows to enter an input/command on the command line
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-s", "--search", type=str)
+        parser.add_argument("-l", "--location", type=str)
+        args = parser.parse_args()
+
+        if args.location and args.search:
+                search_for = f'{args.search}  {args.location}'
+        else:
+                search_for = 'dentist new york'
+        main()
